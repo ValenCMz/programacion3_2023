54f6700751c03566c413838caba299b64ba0a869
Valentin <valentincaminos12345@gmail.com>
2023-04-19 20:39:00 -0300

    👀Ejercicio 4 de tp2 arboles realizado, falta corregir

---
+++ programacion3/TrabajoPractico2.1_Arboles/Ejercicio4/ejercicio4.java
@@ -0,0 +1,40 @@
+package Ejercicio4;
+//Ejercicio 4
+//Se posee un árbol binario (no de búsqueda), donde los nodos internos están vacíos, mientras
+//que las hojas tienen valores enteros. Se debe implementar un método que recorra el árbol y
+//coloque valores en los nodos vacíos (los nodos internos). El valor de cada nodo interno debe
+//ser igual al valor de su hijo derecho, menos el valor de su hijo izquierdo. En caso de que el
+//nodo tenga un solo hijo, el valor del hijo faltante se reemplaza por un 0. Por ejemplo, tomando
+//como entrada el árbol de la izquierda, el árbol resultante debería quedar con los mismos
+//valores que el de la derecha.
+
+//ACLARACION
+//El metodo no va a funcionar ya que es basado en un arbol binario que no es de busqueda y no se implemento todavia ese tipo de arbol
+//me vaso en algunos metodos que tendria en un arbol binario de busqueda
+
+public class ejercicio4 {
+
+//	public void rellenarArbol() {
+//		if(this.left!=null) {
+//			this.left.rellenarArbol();
+//		}
+//		if(this.right!=null) {
+//			this.right.rellenarArbol();
+//		}
+//		
+//		//caso 1
+//		if(this.left!=null&&this.right==null) {
+//			this.value = this.left.getValue();
+//		}
+//		if(this.left==null&&this.right!=null) {
+//			this.value = this.right.getValue();
+//		}
+//		
+//		if(this.left!=null&&this.right!=null) {
+//			this.value = this.right.getValue()-this.left.getValue();
+//		}
+//		
+//		
+//	}
+
+}
